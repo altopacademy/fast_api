8177857b88b80fca31b8583d98642331752ffb4a
helloaltop <helloaltop@gmail.com>
2021-12-22 19:27:27 +0700

    update

--- main.py
+++ main.py
@@ -1,5 +1,39 @@
 from fastapi import FastAPI
 
+import random
+import json
+from fastapi.params import Depends
+
+import torch
+
+from model import NeuralNet
+from nltk_utils import bag_of_words, tokenize
+from pydantic import BaseModel
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+with open('intents.json', 'r') as json_data:
+    intents = json.load(json_data)
+    
+class Item(BaseModel):
+    pertanyaan: str
+
+FILE = "data.pth"
+data = torch.load(FILE)
+
+input_size = data["input_size"]
+hidden_size = data["hidden_size"]
+output_size = data["output_size"]
+all_words = data['all_words']
+tags = data['tags']
+model_state = data["model_state"]
+
+model = NeuralNet(input_size, hidden_size, output_size).to(device)
+model.load_state_dict(model_state)
+model.eval()
+
+bot_name = "Sam"
+
 app = FastAPI()
 
 inventory = {
@@ -21,3 +55,31 @@ def about():
 @app.get("/get-item/{item_id}")
 def get_item(item_id: int):
     return inventory[item_id]
+
+
+@app.get("/tanya/")
+def respon(pertanyaan: str):
+    sentence = tokenize(pertanyaan)
+    X = bag_of_words(sentence, all_words)
+    X = X.reshape(1, X.shape[0])
+    X = torch.from_numpy(X).to(device)
+
+    output = model(X)
+    _, predicted = torch.max(output, dim=1)
+
+    tag = tags[predicted.item()]
+
+    probs = torch.softmax(output, dim=1)
+    prob = probs[0][predicted.item()]
+    if prob.item() > 0.75:
+        for intent in intents['intents']:
+            if tag == intent["tag"]:
+                print(f"{bot_name}: {random.choice(intent['responses'])}")
+                return {"Jawaban": (intent['responses'])}
+    else:
+        print(f"{bot_name}: I do not understand...")
+        return {"Jawaban": "Tidak TAU"}
+    
+    
+    
+    
